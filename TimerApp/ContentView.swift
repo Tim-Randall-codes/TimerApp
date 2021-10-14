d20b52edbd0e6dc89d2089fe606064c54b75f7ef
Tim Randall <trtimrandall@gmail.com>
2021-10-14 22:59:29 +1100

    experiment for maths garden

--- TimerApp/ContentView.swift
+++ TimerApp/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var seconds: Int = 60
+    @State var pause: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text(String(seconds))
+                .padding()
+            Button(action:{
+                pause = false
+                countDown()
+            }, label:{
+                Text("Start timer").padding()
+            })
+            Button(action:{
+                if pause == false {
+                    pause = true
+                }
+                else {
+                    pause = false
+                    countDown()
+                }
+            }, label: {
+                Text("pause").padding()
+            })
+            Button(action:{
+                pause = true
+                seconds = 60
+            }, label: {
+                Text("stop/reset").padding()
+            })
+        }
+    }
+    func countDown(){
+        //this function will subtract one from seconds each second, the @state variable will
+        //help display this. It will go into negatives after zero.
+        let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            seconds -= 1
+            if seconds == 0 {
+                timer.invalidate()
+            }
+            if pause == true {
+                timer.invalidate()
+            }
+        }
     }
 }
 
