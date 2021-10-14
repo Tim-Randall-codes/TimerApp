142fe0caa6d0ab8286e04f16b2fffef915d6ca7e
Tim Randall <trtimrandall@gmail.com>
2021-10-14 21:12:21 +1100

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Tim Randall on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
